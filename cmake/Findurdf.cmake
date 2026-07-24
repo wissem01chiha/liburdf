b743544ac5829f99ee76fc58ceed058d59196e61
wissem chiha <chihawissem08@gmail.com>
2026-07-24 19:07:41 +0100

    feat: add Findurdf CMake module for locating the urdf manipulation library

---
+++ cmake/Findurdf.cmake
@@ -0,0 +1,36 @@
+# SPDX-FileCopyrightText: 2024-2026 Wissem Chiha
+# SPDX-License-Identifier: MIT
+# Distributed under the MIT License.
+#[=======================================================================[.rst:
+Findurdf
+-----------
+
+Find the urdf manipulation library
+
+.. code-block:: cmake
+
+  find_package(urdf [<version>] [...])
+
+Imported Targets
+^^^^^^^^^^^^^^^^
+
+The following targets are imported into the project:
+
+    - ``urdf::urdf``
+
+Result Variables
+^^^^^^^^^^^^^^^^
+This module defines the following variables:
+
+  ``URDF_INCLUDE_DIR``
+    The directory containing the liburdf headers.
+
+  ``URDF_LIBRARY``
+    The liburdf libraries to link against.
+
+  ``URDF_VERSION``
+    The liburdf library to link against in release builds.
+
+  ``URDF_FOUND``
+    True if the liburdf library was found.
+#]=======================================================================]
